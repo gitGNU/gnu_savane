59f8044fb54e3ee777b53992000a293f1ae7216d
Jonathan Gonzalez V <zeus@lunareja.(none)>
2009-05-14 11:41:23 -0400

    Initial import with the django framework

---
+++ savane/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+                       (r'^$', "main.views.index"),
+                       (r'^css/(?P<path>.*)$', 'django.views.static.serve',
+                        {'document_root' : settings.SAVANE_ROOT + 'media/css/'}),
+                       (r'^images/(?P<path>.*)$', 'django.views.static.serve',
+                        {'document_root' : settings.SAVANE_ROOT + 'media/images/'}),
+
+                       # Example:
+                           # (r'^savane/', include('savane.foo.urls')),
+
+                       # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+                       # to INSTALLED_APPS to enable admin documentation:
+                           # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                       # Uncomment the next line to enable the admin:
+                           # (r'^admin/(.*)', admin.site.root),
+                       )
