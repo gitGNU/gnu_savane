50df4e52321fa3a7853609512bb9211135bf733f
Jonathan Gonzalez V <zeus@lunareja.(none)>
2009-05-21 16:51:22 -0400

    Modified the model, now use all the fields from the auth contrib app and the rest of field from table user, so we need a better name fro the model now

--- savane/savane_user/models.py
+++ savane/savane_user/models.py
@@ -1,24 +1,25 @@
 from django.db import models
+#from django.db.models import Manager
+from django.contrib.auth.models import User, UserManager
+#from django.contrib.auth.models import User
 
-class User(models.Model):
-    user_id = models.IntegerField(primary_key=True)
-    user_name = models.TextField()
-    email = models.TextField()
-    user_pw = models.CharField(max_length=96)
+class User(User):
     realname = models.CharField(max_length=96)
     status = models.CharField(max_length=48)
     spamscore = models.IntegerField(null=True, blank=True)
-    add_date = models.IntegerField()
-    confirm_hash = models.CharField(max_length=96, blank=True)
-    authorized_keys = models.TextField(blank=True)
+    confirm_hash = models.CharField(max_length=96, blank=True, null=True)
+    authorized_keys = models.TextField(blank=True, null=True)
     authorized_keys_count = models.IntegerField(null=True, blank=True)
-    email_new = models.TextField(blank=True)
-    people_view_skills = models.IntegerField()
+    people_view_skills = models.IntegerField(null=True)
     people_resume = models.TextField()
-    timezone = models.CharField(max_length=192, blank=True)
-    theme = models.CharField(max_length=45, blank=True)
-    email_hide = models.CharField(max_length=9, blank=True)
-    gpg_key = models.TextField(blank=True)
+    timezone = models.CharField(max_length=192, blank=True, null=True)
+    theme = models.CharField(max_length=45, blank=True, null=True)
+    email_hide = models.CharField(max_length=9, blank=True, null=True)
+    gpg_key = models.TextField(blank=True, null=True)
     gpg_key_count = models.IntegerField(null=True, blank=True)
-    class Meta:
+    objects = UserManager()
+
+    class Meta():
         db_table = u'user'
+
+
