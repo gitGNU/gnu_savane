af1360d6a379b1a2695b986ff0c077aa5c2fa020
Jonathan Gonzalez V <zeus@lunareja.(none)>
2009-06-23 12:50:10 -0400

    Added the primary view for ssh and gpg keys

--- savane/savane_user/views.py
+++ savane/savane_user/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django import forms
+from savane_user.models import User
 
 def index( request ):
     return render_to_response( 'savane_user/index.html',
@@ -89,8 +90,33 @@ def sv_resume_skill( request ):
                                                ) )
 
 def sv_ssh_gpg( request ):
+
+    error_msg = None
+    success_msg = None
+
+    form_gpg = GPGForm()
+    form_ssh = SSHForm()
+    form = None
+
+    if request.method == 'POST':
+        action = request.POST['action']
+        if action == 'update_ssh':
+            form_ssh = SSHForm( request.POST )
+            form = form_ssh
+        elif action == 'update_gpg':
+            form_gpg = GPGForm( request.POST )
+
+        if form is not None and form.is_valid():
+            if action == 'update_ssh':
+                pass
+            elif action == 'update_gpg':
+                pass
+
     return render_to_response( 'savane_user/ssh_gpg.html',
                                RequestContext( request,
+                                               { 'form_gpg' : form_gpg,
+                                                 'form_ssh' : form_ssh,
+                                                 }
                                                ) )
 
 class MailForm( forms.Form ):
@@ -112,3 +138,15 @@ class IdentityForm( forms.Form ):
     name = forms.CharField( required = True )
     last_name = forms.CharField( required = False )
     action = forms.CharField( widget=forms.HiddenInput, required=True, initial='update_identity' )
+
+class GPGForm( forms.Form ):
+    gpg_key = forms.CharField( widget=forms.Textarea, required=False )
+    action = forms.CharField( widget=forms.HiddenInput, required=True, initial='update_gpg' )
+
+class SSHForm( forms.Form ):
+    key_1 = forms.CharField( widget=forms.TextInput( attrs={'size':'60'} ), required=False )
+    key_2 = forms.CharField( widget=forms.TextInput( attrs={'size':'60'} ), required=False )
+    key_3 = forms.CharField( widget=forms.TextInput( attrs={'size':'60'} ), required=False )
+    key_4 = forms.CharField( widget=forms.TextInput( attrs={'size':'60'} ), required=False )
+    key_5 = forms.CharField( widget=forms.TextInput( attrs={'size':'60'} ), required=False )
+    action = forms.CharField( widget=forms.HiddenInput, required=True, initial='update_ssh' )
