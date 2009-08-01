84cb87f6346ac1cf7b54f48ce87e6313e8c35baa
Sylvain Beucler <beuc@beuc.net>
2009-08-01 19:19:27 +0200

    Clarify how to use the migration script and add a copyright/license notice

--- migrate_old_savane.sql
+++ migrate_old_savane.sql
@@ -1,3 +1,27 @@
+-- Convert old Savane data to the new version
+-- 
+-- Copyright (C) 2009  Sylvain Beucler
+-- Copyright (C) 2009  Jonathan Gonzalez V.
+--
+-- This file is part of Savane.
+-- 
+-- Savane is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+-- 
+-- Savane is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Affero General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Affero General Public
+-- License along with this program.  If not, see
+-- <http://www.gnu.org/licenses/>.
+
+-- This script works on the database from 'savane-cleanup' version
+-- with the LATEST DATABASE UPGRADES (check updates/clean-up/).
+
 -- Some clean-up is done on the savane_old database.  It may sound
 -- better to leave savane_old read-only, but at the same time this
 -- means we can experiment the clean-ups on live "old savane" installs
