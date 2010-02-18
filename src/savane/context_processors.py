618a5d2c9a2d2e619b723d40e57a07ce0a3dba5e
Sylvain Beucler <beuc@beuc.net>
2010-02-18 21:20:58 +0100

    Reuse default context processors

--- src/savane/context_processors.py
+++ src/savane/context_processors.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import settings
+from django.conf import settings
 
 def media(request):
     """
