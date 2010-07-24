56849b30c1c6c3dfcd935a071e51d0296ff0c70f
Sylvain Beucler <beuc@beuc.net>
2010-07-24 17:49:08 +0200

    Simplify {% svpagemenu %} call

--- savane/svmain/templatetags/svpagemenu.py
+++ savane/svmain/templatetags/svpagemenu.py
@@ -18,21 +18,26 @@
 
 from django import template
 from django.core.urlresolvers import reverse
+import savane.svmain.models as svmain_models
 
 register = template.Library()
 
 @register.inclusion_tag('svmain/svpagemenu.html', takes_context=True)
-def svpagemenu(context):
-    if context['menu_name'] == 'group':
+def svpagemenu(context, menu_name):
+    if menu_name == 'group':
         entries = []
         entry_home = { 'text' : 'Home',
-                   'href' : reverse('savane.svmain.group_detail', args=[context['group']]),
+                   'href' : reverse('savane.svmain.group_detail', args=[context['group'].name]),
                    'title': "Project Main Page at %s" % 'this website'}
         entry_home['children'] = []
         entry_home['children'].append({'text' : 1.1, 'href' : 1.1, 'title': 1.1 })
         if (context['user'].groups.filter(name=context['group']).count()):
             entry_home['children'].append({'separator' : True })
             entry_home['children'].append({'text' : "I'm a member", 'href' : 1.2, 'title': 1.2 })
+        if (svmain_models.Membership.objects.filter(user=context['user'], group=context['group'], admin_flags='A').count()):
+            entry_home['children'].append({'separator' : True })
+            entry_home['children'].append({'text' : "I'm an admin",
+                                           'href' : reverse('savane.svmain.group_admin', args=[context['group'].name]) })
 
         entry_test = {
                     'text' : 2, 'href' : 2, 'title': 2, 'children':
@@ -43,12 +48,12 @@ def svpagemenu(context):
         entries.append(entry_home)
         entries.append(entry_test)
         return { 'entries' : entries,
-                 'menu_name': context['menu_name'],
+                 'menu_name': menu_name,
         }
-    elif context['menu_name'] == 'my':
+    elif menu_name == 'my':
         return {
             'entries':
             [
             ],
-            'menu_name': context['menu_name'],
+            'menu_name': menu_name,
         }
