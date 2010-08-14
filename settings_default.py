b17eab6418587d5caab84d6e24d8754d206d15a8
Sylvain Beucler <beuc@beuc.net>
2010-08-14 20:31:23 +0200

    Start of Romanian translation

--- settings_default.py
+++ settings_default.py
@@ -46,6 +46,7 @@ LANGUAGES = (
     ('it',    'Italian'),
     ('ja',    'Japanese'),
     ('pt_BR', 'Brazilian Portuguese'),
+    ('ro',    'Romanian'),
     ('ru',    'Russian'),
     ('sv',    'Swedish'),
 )
