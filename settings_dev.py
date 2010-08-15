b7bddf94fedefa182fd7f9218d0e580f609570ff
Sylvain Beucler <beuc@beuc.net>
2010-08-15 11:44:08 +0200

    Suggest developer settings

---
+++ settings_dev.py
@@ -0,0 +1,22 @@
+# Import sane defaults:
+from settings_default import *
+
+# Don't send e-mails for real, display them on the console:
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Alternatively, local mailbox:
+#EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+#EMAIL_FILE_PATH = '/tmp/app-messages' # change this to a proper location
+# Alternatively: in memory, with an interface at /dev/mail/
+#EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+# Configure database access
+DATABASES = {
+    'default': {
+        'NAME': 'savane',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'savane',
+        'PASSWORD': 'savane',
+    }
+}
+
+# Your other configuration parameters here (cf. settings_default).
