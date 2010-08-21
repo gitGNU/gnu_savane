f06212f56d7f06d3e7ef4c8bb575650321c30f2f
Sylvain Beucler <beuc@beuc.net>
2010-08-21 10:07:11 +0200

    Ask search engines not to follow links with 'next=' params

--- savane/svmain/models.py
+++ savane/svmain/models.py
@@ -81,7 +81,6 @@ responsive.
 from django.db import models
 from django.contrib.auth import models as auth_models
 from django.utils.translation import ugettext, ugettext_lazy as _
-from django.utils.safestring import mark_safe
 import datetime
 from savane.utils import markup
 
